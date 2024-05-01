e2f3d1ac4298f75a71a216a41d7c10b8d4341bdd
chandankumarbarnwal <chandanbarnwal111@gmail.com>
2024-05-01 07:26:24 +0530

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3030;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!- running jenkins - running docker on aws ec2');
+  res.send('15.207.94.247:8080 running jenkins - running docker on aws ec2');
 });
 
 const server = app.listen(port, () =>
